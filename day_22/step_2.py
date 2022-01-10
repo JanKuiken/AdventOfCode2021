88bda40e493eeefaa5a254c8de1282cdf2525890
Jan Kuiken <jan.kuiken@quicknet.nl>
2022-01-10 20:38:47 +0100

    day_22, part 2

---
+++ day_22/step_2.py
@@ -0,0 +1,171 @@
+
+# python script for AdventOfCode 2021, day 22, see: https://adventofcode.com/
+
+#  Test mode uses test data and prints some debug info
+test_mode = False
+
+from collections import namedtuple
+from itertools import product, permutations
+
+# read file
+filename = "test_input_2.txt" if test_mode else "input.txt"
+with open(filename) as f:
+    lines = f.readlines()
+
+Line  = namedtuple("line", "min max" )
+Block = namedtuple("Block", "lx ly lz" )
+Rule  = namedtuple("Rule",  "on block")
+
+rules = []
+for line in lines:
+    if line.startswith("#"):
+        continue
+    on_off_str, ranges_str = line.split(" ")
+    on = (on_off_str == "on")
+    x_range_str, y_range_str, z_range_str = ranges_str.split(",")
+    x_range_str = x_range_str[2:]  # strip "x="
+    y_range_str = y_range_str[2:]  # strip "y="
+    z_range_str = z_range_str[2:]  # strip "z="
+    xmin, xmax = x_range_str.split("..")
+    ymin, ymax = y_range_str.split("..")
+    zmin, zmax = z_range_str.split("..")
+    lx = Line(int(xmin), int(xmax))
+    ly = Line(int(ymin), int(ymax))
+    lz = Line(int(zmin), int(zmax))
+    block = Block(lx, ly, lz)
+    rules.append(Rule(on, block))
+
+print("=== part 2 ===")
+
+# - ons belangrijkste data type is een blok met min-x,y,z en max-x,y,z
+# - we gaan blokken opslaan van cubes die 'on' zijn.
+# - rules werken we van boven naar beneden af
+# - bij een 'off' rule moeten we kijken naar overlap
+#   - een on kan 'opgeslokt' worden door een off-block (verdwijnt dan)
+#   - bij deelse over wordt het on-blok gesplitst in deelblokken, waarbij
+#        overlapblok wegvalt en de andere overblijven
+# - bij een 'on' rule moeten we ook naar overlap kijken, anders krijgen we
+#        bij de eind-adminstratie dubbel tellingen, maar hoe ???
+
+
+# oke dus:
+# - speel terrein is ongeveer(-1e5..+1e5, -1e5..+1e5, -1e5..+1e5), maw ongeveer 10^15 cubes...
+# - we hebben meer on-rules dan off rules
+# - blokken zijn tot ca. 40k hoog/breed/lang en vanaf ongeveer 10
+
+# paniek-status-gevoel nog steeds hoog, wellicht wordt dat verlicht als we eerst naar 
+# een 1D situatie bekijken
+
+def overlap_and_remainders(base, overlap):
+    """
+    Determine if and how a 1D line ('overlap') overlaps another ('base')
+    Returns:
+      - overlapped region
+      - list of remaining regions of 'base'
+      - list of remaining regions of 'overlap'
+    """
+    # no overlap
+    if overlap.max < base.min or overlap.min > base.max:
+        return None, [base], [overlap]
+    # exact overlap
+    if overlap.min == base.min and overlap.max == base.max:
+        return base, [], []
+    # completely inside
+    if overlap.min > base.min and overlap.max < base.max:
+        return overlap,                                                       \
+               [Line(base.min, overlap.min-1), Line(overlap.max+1, base.max)],\
+               []
+
+    # completely outside
+    if overlap.min < base.min and overlap.max > base.max:
+        return base,                                                          \
+               [],                                                            \
+               [Line(overlap.min, base.min-1), Line(base.max+1, overlap.max)]
+
+    # right exact match
+    if overlap.max == base.max:
+        if overlap.min < base.min:
+            return base,                                        \
+                   [],                                          \
+                   [Line(overlap.min, base.min-1)] 
+        else:    # overlap.min > base.min
+            return Line(overlap.min, base.max),                 \
+                   [Line(base.min, overlap.min-1)],             \
+                   []
+
+    # left exact match
+    if overlap.min == base.min:
+        if overlap.max > base.max:
+            return base,                                        \
+                   [],                                          \
+                   [Line(base.max+1, overlap.max)]
+        else: # overlap.max < base.max
+            return Line(base.min, overlap.max),                 \
+                   [Line(overlap.max+1, base.max)],             \
+                   []
+
+    # left overlap
+    if overlap.max < base.max:
+        return Line(base.min, overlap.max),                     \
+               [Line(overlap.max+1, base.max)],                 \
+               [Line(overlap.min, base.min-1)] 
+
+    # right overlap
+    if overlap.min > base.min:
+        return Line(overlap.min, base.max),                    \
+               [Line(base.min, overlap.min-1)],                \
+               [Line(base.max+1, overlap.max)]
+    
+    # did i miss a situation? :
+    raise NotImplementedError("unhandled situation, notify developer")
+
+
+
+def apply_rule(rule):
+    global all_on_blocks
+
+    # empty list (at startup.... if every cube is turned off)
+    if len(all_on_blocks) == 0 and rule.on:
+        all_on_blocks.append(rule.block)
+        return # we're done
+
+    # we copy every thing to a new list and then swap them back
+    new_on_blocks = []
+    for on_block in all_on_blocks:
+        # determine overlap in three directions
+        overlap_x, remainders_x, _ = overlap_and_remainders(on_block.lx, rule.block.lx)
+        overlap_y, remainders_y, _ = overlap_and_remainders(on_block.ly, rule.block.ly)
+        overlap_z, remainders_z, _ = overlap_and_remainders(on_block.lz, rule.block.lz)
+        # check if there is overlap in all directions
+        if overlap_x and overlap_y and overlap_z:
+            # first add all sectioned blocks 
+            # (make a list for overlaps so that we can add it to remainders)
+            for lx,ly,lz in product( [overlap_x] +  remainders_x,
+                                     [overlap_y] +  remainders_y,
+                                     [overlap_z] +  remainders_z,  ) :
+                new_on_blocks.append(Block(lx, ly, lz))
+            # then remove overlapped block again
+            new_on_blocks.remove(Block(overlap_x, overlap_y, overlap_z))
+        else:
+            new_on_blocks.append(on_block)
+    # if we had an on rule we have to add the block off the rule
+    if rule.on:
+        new_on_blocks.append(rule.block)
+
+    # copy new blocks into our global variable
+    all_on_blocks = new_on_blocks
+
+def number_of_on_cubes_inside_block(block):
+    return   (1 +  block.lx.max - block.lx.min)   \
+           * (1 +  block.ly.max - block.ly.min)   \
+           * (1 +  block.lz.max - block.lz.min)
+
+# initialize our memory
+all_on_blocks = []
+
+for no, rule in enumerate(rules):
+    print(no, len(all_on_blocks), rule)   
+    apply_rule(rule)
+
+print("\n\nAnswer", sum([number_of_on_cubes_inside_block(b) for b in all_on_blocks]))
+
